eabd0b78a828a7f2be0ac15cf1edb6d8c8b3a415
Suleiman Tijjani Shehu <suleimanshehutijjani@gmail.com>
2023-03-13 09:41:09 -0700

    Andrew key board

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-if __name__ = "__main__":
-    import sys
-    import hidden_4
-for n in dir(hidden_4):
-if n[:2] != "__":
-    print(n)
+
+
+if __name__ == "__main__":
+
+    """Print the value of variable a from variable_load_5."""
+
+    from variable_load_5 import a
+
+
+    print(a)
